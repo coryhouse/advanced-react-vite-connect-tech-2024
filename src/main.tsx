fd015c6ee6edefe3a5df165f15f689476f1d4490
Cory House <housecor@gmail.com>
2024-11-18 13:37:59 -0500

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -6,10 +6,24 @@ import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary fallback={<h1>Sorry an error occurred.</h1>}>
+          <Suspense fallback={<p>Loading...</p>}>
+            <App />
+          </Suspense>
+        </ErrorBoundary>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
